955b20d784244068e20cd62956ad54ba9bbda804
datata <dani@danitarazona.dev>
2024-05-10 19:29:07 +0200

    update: refactor to controller

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-const express = require('express')
+const express = require('express');
+const userController = require('./controllers/userController');
+
 // import express from "express";
 
 const app = express();
@@ -7,38 +9,14 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 4000;
 
-app.post('/api/users', (req, res) => {
-
-  console.log('-------------------');
-  console.log(req.body);
-  console.log('-------------------');
-
-  // recuperar la info a guardar en bd
-  // Validaciones
-  // tratar la informacion
-  // guardar en bd
-  // finalmente siempre mi api debe devolver una respuesta
-
-
-  // res.status(200).send('CREATE USER')
-})
-
-app.get('/api/users', (req, res) => {
-  res.status(200).send('GET USERS')
-})
-
-app.put('/api/users', (req, res) => {
-
-  console.log('-------------------');
-  console.log(req.body);
-  console.log('-------------------');
-
-  res.status(200).send('UPDATE USER')
-})
-
-app.delete('/api/users/:id', (req, res) => {
-  res.status(200).send('DELETE USER ' + req.params.id)
-})
+// crear un usuario
+app.post('/api/users', userController.createUser)
+// recuperar todos los usuarios
+app.get('/api/users', userController.getUsers)
+// actulizar un user
+app.put('/api/users', userController.updateUser)
+// eliminar user
+app.delete('/api/users/:id', userController.deleteUser)
 
 app.listen(PORT, () => {
   // console.log("Server is running on port" + PORT);
