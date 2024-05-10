1b87f978f06b1e778013ce0f297bb7bd76bd48e6
datata <dani@danitarazona.dev>
2024-05-10 21:19:36 +0200

    delete: delete some mehtod User class

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,76 +3,76 @@ const User = require("../database/models/User");
 
 const userController = {};
 
-userController.createUser = (req, res) => {  
-  // recuperar la info a guardar en bd
-  const name = req.body.name
-  const email = req.body.email
-  const password = req.body.password
-  const isActive = 1
-  const roleId = 1
-  // Validaciones
+userController.createUser = (req, res) => {
+	// recuperar la info a guardar en bd
+	const name = req.body.name;
+	const email = req.body.email;
+	const password = req.body.password;
+	const isActive = 1;
+	const roleId = 1;
 
-  // tratar la informacion como por ejemplo encriptar la contraseña
+	// Validaciones
+	if (!name || !email || !password) {
+		return res.status(400).json("Name, email and password are required");
+	}
 
-  // guardar en bd
-  User.create(
-    {
-      name: name,
-      email: email,
-      password: password,
-      isActive: isActive,
-      roleId: roleId
-    }
-  )
-  
-  // finalmente siempre mi api debe devolver una respuesta
-  res.status(201).json('CREATE USER')
-}
+	// tratar la informacion como por ejemplo encriptar la contraseña
+
+	// guardar en bd
+	User.create({
+		name: name,
+		email: email,
+		password: password,
+		isActive: isActive,
+		roleId: roleId,
+	});
+
+	// finalmente siempre mi api debe devolver una respuesta
+	res.status(201).json("CREATE USER");
+};
 
 userController.getUsers = (req, res) => {
-  // consultar en la base de datos
-  const users = User.find();
+	// consultar en la base de datos
+	const users = User.find();
 
-  res.status(200).json(users)
-}
+	res.status(200).json(users);
+};
 
-userController.updateUser =  (req, res) => {
+userController.updateUser = (req, res) => {
+	console.log("-------------------");
+	console.log(req.body);
+	console.log("-------------------");
 
-  console.log('-------------------');
-  console.log(req.body);
-  console.log('-------------------');
-
-  res.status(200).send('UPDATE USER')
-}
+	res.status(200).send("UPDATE USER");
+};
 
 userController.deleteUser = (req, res) => {
-  // recuperar el id del usuario a eliminar
-  const userIdToDelete = req.params.id
+	// recuperar el id del usuario a eliminar
+	const userIdToDelete = req.params.id;
 
-  // eliminar de la db el usuario por id
-  const userDeleted = User.delete(userIdToDelete)
+	// eliminar de la db el usuario por id
+	const userDeleted = User.delete(userIdToDelete);
 
-  if(userDeleted) {
-    res.status(200).json('DELETE USER ' + req.params.id)
-  } else {
-    res.status(404).json('User doesnt exist: ' + req.params.id)
-  }
-}
+	if (userDeleted) {
+		res.status(200).json("DELETE USER " + req.params.id);
+	} else {
+		res.status(404).json("User doesnt exist: " + req.params.id);
+	}
+};
 
 userController.findUserById = (req, res) => {
-  // recuperar el id del usuario que quieto recupear
-  const userId = req.params.id
+	// recuperar el id del usuario que quieto recupear
+	const userId = req.params.id;
 
-  // recuperar el usuario de la base de datos a traves del modelo
-  const user = User.findById(userId)
+	// recuperar el usuario de la base de datos a traves del modelo
+	const user = User.findById(userId);
 
-  // finalmante devolvemos una respuesta
-  if(!user) {
-    res.status(404).json('User doesnt exists')
-  } else {
-    res.status(200).json(user)
-  }
-}
-
+	// finalmante devolvemos una respuesta
+	if (!user) {
+		res.status(404).json("User doesnt exists");
+	} else {
+		res.status(200).json(user);
+	}
+};
 
 module.exports = userController;
--- src/database/models/User.js
+++ src/database/models/User.js
@@ -1,4 +1,3 @@
-const { log } = require("console");
 const db = require("../db");
 
 class User {
@@ -9,50 +8,6 @@ class User {
 	#isActive;
 	#roleId;
 	static idCounter = 1;
-
-	get id() {
-		return this.#id;
-	}
-
-	get name() {
-		return this.#name;
-	}
-
-	set name(name) {
-		this.#name = name;
-	}
-
-	get email() {
-		return this.#email;
-	}
-
-	set email(email) {
-		this.#email = email;
-	}
-
-	get password() {
-		return this.#password;
-	}
-
-	set password(password) {
-		this.#password = password;
-	}
-
-	get isActive() {
-		return this.#isActive;
-	}
-
-	set isActive(isActive) {
-		this.#isActive = isActive;
-	}
-
-	get roleId() {
-		return this.#roleId;
-	}
-
-	set roleId(roleId) {
-		this.#roleId = roleId;
-	}
 
 	static create(data) {
 		const user = { id: User.idCounter++, ...data };
